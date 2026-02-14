c0d15dacaf82ecec6bc18987675436dc2a53452b
郭申申 <3501641633@qq.com>
2026-02-14 18:46:16 +0800

    实现客户端的写入

--- my/client.cpp
+++ my/client.cpp
@@ -2,6 +2,17 @@
 #include <arpa/inet.h>
 #include <cstring>
 #include <cstdio>
+#include <stdlib.h>
+#include <unistd.h>
+
+void errif(bool condition, const char *errmsg)
+{
+    if (condition)
+    {
+        perror(errmsg);
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main()
 {
@@ -14,5 +25,36 @@ int main()
     serv_addr.sin_port = htons(8888);
 
     connect(sockfd, (sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    while (true)
+    {
+        char buffer[1024];
+        bzero(buffer, sizeof(buffer));
+        scanf("%s", buffer);
+        ssize_t write_bytes = write(sockfd, buffer, strlen(buffer));
+        if (write_bytes < 0)
+        {
+            printf("write error\n");
+            close(sockfd);
+            break;
+        }
+        bzero(buffer, sizeof(buffer));                             // 清空缓冲区
+        ssize_t read_bytes = read(sockfd, buffer, sizeof(buffer)); // 从服务器socket读到缓冲区，返回已读数据大小
+        if (read_bytes > 0)
+        {
+            printf("message from server: %s\n", buffer);
+        }
+        else if (read_bytes == 0)
+        { // read返回0，表示EOF，通常是服务器断开链接，等会儿进行测试
+            printf("server socket disconnected!\n");
+            break;
+        }
+        else if (read_bytes == -1)
+        { // read返回-1，表示发生错误，按照上文方法进行错误处理
+            close(sockfd);
+            errif(true, "socket read error");
+        }
+    }
+
     return 0;
 }
--- my/server.cpp
+++ my/server.cpp
@@ -2,10 +2,22 @@
 #include <arpa/inet.h>
 #include <cstring>
 #include <cstdio>
+#include <stdlib.h>
+#include <unistd.h>
+
+void errif(bool condition, const char *errmsg)
+{
+    if (condition)
+    {
+        perror(errmsg);
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main()
 {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    errif(sockfd == -1, "socket create error");
     struct sockaddr_in serv_addr;
     struct sockaddr_in client_addr;
     bzero(&serv_addr, sizeof(serv_addr));
@@ -16,12 +28,35 @@ int main()
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv_addr.sin_port = htons(8888);
 
-    bind(sockfd, (sockaddr *)&serv_addr, sizeof(serv_addr));
+    int bind_result = bind(sockfd, (sockaddr *)&serv_addr, sizeof(serv_addr));
+    errif(bind_result == -1, "socket bind error");
 
-    listen(sockfd, SOMAXCONN);
+    int listen_result = listen(sockfd, SOMAXCONN);
+    errif(listen_result == -1, "socket listen error");
 
     int client_sockfd = accept(sockfd, (sockaddr *)&client_addr, &clnt_addr_len);
+    errif(client_sockfd == -1, "socket accept error");
     printf("new client fd %d! IP: %s Port: %d\n", client_sockfd, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+    while (true)
+    {
+        char buffer[1024];
+        bzero(buffer, sizeof(buffer));
+        ssize_t read_bytes = read(client_sockfd, buffer, sizeof(buffer));
+        if (read_bytes == 0)
+        {
+            printf("client fd %d closed\n", client_sockfd);
+            close(client_sockfd);
+            break;
+        }
+        else if (read_bytes < 0)
+        {
+            perror("read error");
+            close(client_sockfd);
+            break;
+        }
+        write(client_sockfd, buffer, strlen(buffer));
+    }
 
     return 0;
 }
